e896c25b287401eae995d0d62ae4ed230094ef04
Jaime Garcia <luna@MacBook-Pro-de-Jaime.local>
2018-10-15 17:44:27 +0200

    karaoke sin pep8

--- karaoke.py
+++ karaoke.py
@@ -3,28 +3,59 @@
 
 
 import sys 
+import json
+import urllib
 from Smallsmillhandler import SmallSMILHandler 
 from xml.sax import make_parser 
 from xml.sax.handler import ContentHandler
 
-def new_list(fichero):
-        parser = make_parrser()
+
+class KaraokeLocal(SmallSMILHandler)
+    def __init__(self, fichero):
+        parser = make_parser()
         cHandler = SmallSMILHandler()
-        parser.setContentHandler(cHandler)
-        parser.parse(fichero)
-        list = cHandler.get_tags()
-        return list
-    
-def print_list(list)
-
-
-def open_fichero():
-    try:
-        fichero = open(sys.argv[1],'r')
-        return fichero
-    except IndexError:
-        sys.exit("Usage:python3 Karaoke.py file.smil")
+        parser.setContenHandler(cHandler)
+        parser.parse(open(fichero))
+        sewlf.Tunk = cHandler.get_tags()
         
-if __name__ == "__main__":
-    
-    
+    def do_local(self):
+        """
+        archivos dentro de las URLs
+        """
+        doc = ""
+        for list_trunk in self.Trunk
+            for key in dic_trunk:
+                doc = doc + key + "\t"
+                for key_2 in div_trunk[key]:
+                    if str(list_trunk [key][key_2][0:7] == "http://")
+                        urlc = list_trunk[key][key_2] = url[url.rfind("/")+1]
+                        urlretrieve(url)
+
+    def __str__(self):
+        doc = ""
+        for list.trunk in self.Trunk:
+            for key in list_trunk:
+                doc = doc + key + "\t"
+                for key_2 in list_trunk[key]:
+                    doc = doc + key_2 +'="' + list_trunk[key][key_2] + '"' + "\t"
+                    doc = doc + "\n"
+        print(doc)
+        return(doc)
+
+    def to_json(self, fich):
+        archivo_json = open('karaoke.json', 'w')
+        json.dump(lista, archivo_json, sort_keys = true, indent = 4, separators = (' ', ': '))
+        archivo_json.close()
+                
+        
+if __name__=="__main__":
+    try:
+        fichero = sys.argv[1]
+        Fich = KaraokeLocal(fichero)
+    except:
+        sys.exit("Usage: python3 karaoke.py file smil")
+    Fich_json =sys.argv[1][:-5] + ".json"
+    Fich.__str__()
+    fich.to_json(Fich_json)
+    fich.do_local(Fich_json, "local.json")
+    fich.__str__()
