74a966a68a78aa017069169f0f0ace156c0cb9c7
Jaime Garcia <luna@MacBook-Pro-de-Jaime.local>
2018-10-15 16:58:46 +0200

    smallsmilhandler sin termina

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -5,6 +5,7 @@
 from xml.sax import make_parser 
 from xml.sax.handler import ContentHandler 
 
+
 class SmallSMILHandler(ContentHandler)
 
     def __init__(self):
@@ -16,30 +17,29 @@ class SmallSMILHandler(ContentHandler)
                         'region':['id', 'top','bottom', 'left', 'right'],
                         'img':['src', 'begin', 'dur'],
                         'textstream':['src', 'region']}
-        self.atr{}
+        
         
     def startElement(self, name, attrs):
     """
     Metodo para abrir etiqueta 
     """
     
-    if name == 'self.list':
+    if name in self.list:
         #Asi cogemos todos losatributos de mi diccionario y no tengo que hacer varios if
-        for name in self.etiquetas: #iteramos por el diccionario 
-                self.atr = attrs.get('valor', "")
-    self.nueva_list(name, self.atributo)
+        print(name)
+        for attrs in self.list[name]
+            self.attrs = {}
+            self.attr[valor] = attrs.get(valor, "")
+            self.list.appenjd(self.attrs)
+            
     
     def get_tags(self):
         return self.etiquetas
     
-    def nueva_lista(self, nombre, atributos):
-        etiqueta = []
-        self.etiqueta = self.endetiqueta(name)
-            self.etiqueta = self.ednetiqueta(atributo)
             
     if __name__ == "__main__":
         
-        parser = make_parser()
         cHandler = SmallSMILHandler()
+        parser = make_parser()
         parser.setContentHandler(cHandler)
         parser.parse(open('karaoke.smil'))
