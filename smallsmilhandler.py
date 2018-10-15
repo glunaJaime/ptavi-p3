46ee138c3eb65a815ae634c6eea67077d9587140
Jaime Garcia <luna@MacBook-Pro-de-Jaime.local>
2018-10-15 12:37:23 +0200

    smallsmilhandler incompleto

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -5,15 +5,15 @@
 from xml.sax import make_parser 
 from xml.sax.handler import ContentHandler 
 
-class SmallSmilHandler(ContentHandler)
+class SmallSMILHandler(ContentHandler)
 
     def __init__(self):
     """
     Inicializamos las variables
     """
-        self.etiquetas = {}
-        self.lista = {'root-layout':['widht', 'height', 'background-                                color'],
-                        'region':['id', 'top','bottom', 'left',                 'right'],
+        self.etiquetas = []
+        self.list = {'root-layout':['widht', 'height', 'background-color'],
+                        'region':['id', 'top','bottom', 'left', 'right'],
                         'img':['src', 'begin', 'dur'],
                         'textstream':['src', 'region']}
         self.atr{}
@@ -23,14 +23,23 @@ class SmallSmilHandler(ContentHandler)
     Metodo para abrir etiqueta 
     """
     
-    if name == 'self.lista':
+    if name == 'self.list':
         #Asi cogemos todos losatributos de mi diccionario y no tengo que hacer varios if
         for name in self.etiquetas: #iteramos por el diccionario 
                 self.atr = attrs.get('valor', "")
-                
+    self.nueva_list(name, self.atributo)
+    
+    def get_tags(self):
+        return self.etiquetas
+    
+    def nueva_lista(self, nombre, atributos):
+        etiqueta = []
+        self.etiqueta = self.endetiqueta(name)
+            self.etiqueta = self.ednetiqueta(atributo)
+            
     if __name__ == "__main__":
         
-        parser = makeparser()
-        cHandler = CHandler()
+        parser = make_parser()
+        cHandler = SmallSMILHandler()
         parser.setContentHandler(cHandler)
         parser.parse(open('karaoke.smil'))
