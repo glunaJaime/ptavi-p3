fdeee6b7681bced457c336e144e925c650de9f4d
Jaime Garcia <luna@MacBook-Pro-de-Jaime.local>
2018-10-16 16:25:49 +0200

    karaoke con pep8

--- karaoke.py
+++ karaoke.py
@@ -1,71 +1,62 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 
-import sys 
+import sys
 import json
+from xml.sax import make_parser
+from smallsmilhandler import SmallSMILHandler
 import urllib
-from Smallsmillhandler import SmallSMILHandler 
-from xml.sax import make_parser 
-from xml.sax.handler import ContentHandler
 
 
-class KaraokeLocal(SmallSMILHandler)
-    
-    def __init__(self, fichero):
+class karaokelocal(SmallSMILHandler):
+    def __init__(self):
+        """ Método para capturar de terminal y realizar parseo """
+        try:
+            fich = sys.argv[1]
+        except IndexError:
+            sys.exit("Usage: python3 karaoke.py file.smil")
         parser = make_parser()
-        cHandler = SmallSMILHandler()
-        parser.setContenHandler(cHandler)
-        parser.parse(open(fichero))
-        self.Tunk = cHandler.get_tags()
-        
-    def do_local(self):
-        """
-        archivos dentro de las URLs
-        """
-        doc = ""
-        for list_trunk in self.Trunk
-            for key in dic_trunk:
-                doc = doc + key + "\t"
-                for key_2 in div_trunk[key]:
-                    if str(list_trunk [key][key_2][0:7] == "http://")
-                        urlc = list_trunk[key][key_2] = url[url.rfind("/")+1]
-                        urlretrieve(url)
+        KHandler = SmallSMILHandler()
+        parser.setContentHandler(KHandler)
+        parser.parse(open(fich))
+        self.mytags = KHandler.get_tags()
 
     def __str__(self):
-        doc = ""
-        for list.trunk in self.Trunk:
-            for key in list_trunk:
-                doc = doc + key + "\t"
-                for key_2 in list_trunk[key]:
-                    doc = doc + key_2 +'="' + list_trunk[key][key_2] + '"' + "\t"
-                    doc = doc + "\n"
-        print(doc)
-        return(doc)
+        """ Método para crear la lista de etiquetas """
+        self.final = ""
+        for dic in self.mytags:
+            frase = "\t"
+            for element in dic:
+                for atributo in dic[element]:
+                    frase += atributo + " = '" + dic[element][atributo] + "'\t"
+                self.final += element + frase + "\n"
+        return self.final
 
-    def to_json(self, fich):
-        archivo_json = open('karaoke.json', 'w')
-        json.dump(lista, archivo_json, sort_keys = true, indent = 4, separators = (' ', ': '))
-        archivo_json.close()
-                
-        
-if __name__=="__main__":
-    try:
-        fichero = sys.argv[1]
-        Fich = KaraokeLocal(fichero)
-    except:
-        sys.exit("Usage: python3 karaoke.py file smil")
-    
-    """"
-    print(karaoke)
-    karaoke.do_local()
-    karaoke.to_json(fichero)
-    karaoke.to_json(fichero,'local')
-    print(karaoke)
-    """"
-    Fich_json =sys.argv[1][:-5] + ".json"
-    Fich.__str__()
-    fich.to_json(Fich_json)
-    fich.do_local(Fich_json, "local.json")
-    fich.__str__()
-    
+    def do_json(self, name="local"):
+        if name != "local":
+            name = sys.argv[1].split(".")[0]
+
+        self.name = name + ".json"
+        with open(self.name, "w") as outfile:
+            json.dump(self.mytags, outfile, sort_keys=True, indent=4)
+
+    def do_local(self):
+        for dic in self.mytags:
+            for element in dic:
+                for atributo in dic[element]:
+                    if atributo == 'src':
+                        atr = dic[element][atributo]  # Lo utilizo para acortar
+                        if atr.startswith('http://'):
+                            urllib.request.urlretrieve(atr, atr.split('/')[-1])
+                            dic[element][atributo] = "./" + atr.split('/')[-1]
+
+
+if __name__ == "__main__":
+
+    K_Local = karaokelocal()
+    print(K_Local)
+    K_Local.do_json(sys.argv[1])
+    K_Local.do_local()
+    print(K_Local)
+    K_Local.do_json()
